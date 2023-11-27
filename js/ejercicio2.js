b28590604abee20d7dc0911a9d657e49b272d543
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-27 19:58:04 -0300

    agrego estilo de los btn mostrar, generacion y mayor y sus respectivas fc, usando la clase persona del tp anterior

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,28 +1,78 @@
 import Persona from "./classPersona.js";
 
-const btnEnviar=document.getElementById("enviar");
-console.log(btnEnviar);
+const ingresaPersona = document.querySelector("form");
 
-const ingresaPersona=document.querySelector("form");
+const nombre = document.getElementById("nombre"),
+  edad = document.getElementById("edad"),
+  dni = document.getElementById("dni"),
+  sexo = document.getElementById("sexo"),
+  peso = document.getElementById("peso"),
+  altura = document.getElementById("altura"),
+  nacimiento = document.getElementById("anioNacimiento");
 
-console.log(ingresaPersona);
+const btnMostrar = document.getElementById("mostrar");
+const modalMostrar = document.getElementById("person-ing");
+const btnMayor = document.getElementById("mayor");
+const padre = document.getElementById("padre");
+const btnVolver = document.getElementById("volver");
+const btnGeneracion = document.getElementById("generacion");
 
-const nombre=document.getElementById("nombre"),edad=document.getElementById("edad"),dni=document.getElementById("dni"),sexo=document.getElementById("sexo"),peso=document.getElementById("peso"),altura=document.getElementById("altura"),nacimiento=document.getElementById("anioNacimiento");
+const personaIng = new bootstrap.Modal(document.getElementById("person-ing"));
+const generacion = () => {};
+const mostrarDatos = () => {
+  const parrDatos = document.createElement("p");
 
-const btnMostrar=document.getElementById("mostrar");
+  parrDatos.innerHTML =
+    "Nombre: " +
+    nombre.value +
+    "<br>Edad: " +
+    edad.value +
+    "<br>DNI: " +
+    dni.value +
+    "<br>Sexo: " +
+    sexo.value +
+    "<br>Peso: " +
+    peso.value +
+    "<br>Altura: " +
+    altura.value +
+    "<br>Año de nacimiento: " +
+    nacimiento.value;
+
+  padre.insertBefore(parrDatos, btnVolver);
+
+  personaIng.show();
+};
+
+const mayorEdad = () => {
+  console.log(nacimiento.value);
+  if (nacimiento.value >= 18)
+    alert(nombre.value + " es mayor de edad" + nacimiento.value);
+  else alert(nombre.value + " es menor de edad");
+};
 
 const creaPersona = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const persona= new Persona(nombre.value,edad.value,dni.value,sexo.value,nacimiento.value,peso.value,altura.value);
+  const persona = new Persona(
+    nombre.value,
+    edad.value,
+    dni.value,
+    sexo.value,
+    nacimiento.value,
+    peso.value,
+    altura.value
+  );
 
-    ingresaPersona.reset();
+  ingresaPersona.reset();
 
-    return persona;
-}
+  return persona;
+};
 
 ingresaPersona.addEventListener("submit", creaPersona);
+const pers=creaPersona(); console.log(pers);
 
+btnMostrar.addEventListener("click", mostrarDatos);
 
+btnMayor.addEventListener("click", mayorEdad);
 
-//btnMostrar.addEventListener("click",);
+btnGeneracion.addEventListener("click", generacion);
